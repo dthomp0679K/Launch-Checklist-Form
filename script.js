36df6be070eaea645fee01ac4ffe64a60cbb3047
dthomp0679K <derek_thompson@cable.comcast.com>
2021-06-11 10:28:09 -0500

    new

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   this.fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      response.json().then(function(json) {
+         const div = document.getElementById("missionTaget");
+         const missionDestination = Math.round(Math.random()*5);
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[missionDestination].name}</li>
+            <li>Diameter: ${json[missionDestination].diameter}</li>
+            <li>Star: ${json[missionDestination].star}</li>
+            <li>Distance from Earth: ${json[missionDestination].moons}</li>
+            </ol>
+         `;   
+   });
+});
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
